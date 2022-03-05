ce4e66110996080f741d492bd523c450c4a8ca84
Rashad Nelson <rashadnelson@gmail.com>
2022-03-05 12:45:51 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./reset.css"
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const magazinesData = [
+  {
+    id: 1,
+    image: "https://i1.feedspot.com/200/4918930.jpg?t=1616506424",
+    title: "Men's Health",
+  },
+  {
+    id: 2,
+    image: "https://i1.feedspot.com/200/5313863.jpg?t=1637739821",
+    title: "Man of Many",
+  },
+  {
+    id: 3,
+    image: "https://i1.feedspot.com/200/4922678.jpg?t=1628487234",
+    title: "Maxim Magazine",
+  },
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const MagazinesList = () => {
+  return(
+    <section className="magazinelist">
+      {magazinesData.map((magazineData) => {
+        return(
+          <section>
+            <Magazines
+              key={magazineData.id}
+              magazineData = {magazineData}
+            >
+            </Magazines>
+          </section>
+        )
+      })}
+    </section>
+  )
+}
+
+const Magazines = (props) => {
+
+  const { image, title } = props.magazineData
+
+  return(
+    <section className="magazines">
+      <img src={image} alt="" />
+
+      <h3 className="magazinetitle">{title}</h3>
+
+    </section>
+  )
+}
+
+ReactDOM.render(<MagazinesList/>, document.getElementById("root"))
