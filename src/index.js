09688d0b104ab04a83b31641f560e364c1a2038a
Rashad Nelson <rashadnelson@gmail.com>
2022-04-09 12:15:20 -0400

    Added GQ magazine

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ const magazinesData = [
     title: "Men's Health",
   },
   {
-    id: 1,
+    id: 2,
     image: "https://secure.static.meredith.com/crt/store/covers/magazines/nmo/0400_m.jpg",
     title: "Esquire",
+  },
+  {
+    id: 3,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp2nXFFhUYxupqA57xFguPXKmuvGJDdaHjVQ&usqp=CAU",
+    title: "GQ",
   },
 
 
