5ccc39f9da845da6a9b31348d5aaa249d6341303
Rashad Nelson <rashadnelson@gmail.com>
2022-04-19 08:26:42 -0400

    Removed Maxim Magazine

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ const magazinesData = [
     image: "https://i1.feedspot.com/200/4918930.jpg?t=1616506424",
     title: "Men's Health",
   },
-  {
-    id: 2,
-    image: "https://i1.feedspot.com/200/randomimage.jpg",
-    title: "Maxim",
-  },
 
 ]
 
